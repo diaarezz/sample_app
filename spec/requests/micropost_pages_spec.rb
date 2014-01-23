ac741bc8a4e3a04fc576816751ac2488d91d7fda
diaarezz <your.akuarezz@gmail.com>
2014-01-23 15:32:54 +0800

    Add Css

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -30,7 +30,8 @@ describe "Micropost pages" do
       end
     end
   end
-describe "micropost destruction" do
+  
+  describe "micropost destruction" do
     before { FactoryGirl.create(:micropost, user: user) }
 
     describe "as correct user" do
@@ -38,6 +39,35 @@ describe "micropost destruction" do
 
       it "should delete a micropost" do
         expect { click_link "delete" }.to change(Micropost, :count).by(-1)
+      end
+    end
+
+  describe "as another user" do
+      before do
+        another_user = FactoryGirl.create(:user, email: "another@user.com")
+        visit signin_path
+        valid_signin another_user
+        visit user_path(user)
+      end
+
+      it { should_not have_link('delete', 
+                                href: micropost_path(user.microposts.first.id)) }
+    end
+  end
+
+  describe "micropost display" do
+    before do
+      50.times { FactoryGirl.create(:micropost, user: user) }
+      visit root_path
+    end
+
+    describe "pagination" do
+      it { should have_selector('div.pagination') }
+
+      it "should list each micropost" do
+        user.microposts.paginate(page: 1).each do |m|
+          page.should have_selector("li##{m.id}", text: m.content)
+        end
       end
     end
   end
